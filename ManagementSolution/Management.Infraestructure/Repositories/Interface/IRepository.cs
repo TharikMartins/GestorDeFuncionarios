c133e792aa8456ae38a1f503089619ae68d2ac2a
Tharik Martins <tharikmartins@hotmail.com>
2020-10-28 23:01:30 -0300

    Refatorando o repositorio usando Repository Pattern

---
+++ ManagementSolution/Management.Infraestructure/Repositories/Interface/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Management.Infraestructure.Repositories.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+        List<T> List();
+        void Save(T Entity);
+        void Delete(T Entity);
+        T FindById(int Id);
+        void Update(T Entity);
+
+
+    }
+}
---
+++ ManagementSolution/Management.Infraestructure/Repositories/Repository.cs
@@ -0,0 +1,42 @@
+﻿using Management.Infraestructure.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Management.Infraestructure.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext _dbContext;
+
+        public Repository(DbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public virtual List<TEntity> List()
+        {
+            return this._dbContext.Set<TEntity>().ToList();
+        }
+
+        public virtual void Save(TEntity Entity)
+        {
+            this._dbContext.Set<TEntity>().Add(Entity);
+        }
+
+        public virtual void Delete(TEntity Entity)
+        {
+            this._dbContext.Set<TEntity>().Remove(Entity);
+        }
+
+        public virtual TEntity FindById(int Id)
+        {
+            return this._dbContext.Set<TEntity>().Find(Id);
+        }
+
+        public virtual void Update(TEntity Entity)
+        {
+            this._dbContext.Set<TEntity>().Update(Entity);
+        }
+    }
+}
